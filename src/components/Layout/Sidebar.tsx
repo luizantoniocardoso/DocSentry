eb24f8929b21ff25064e3fbb6d83d5bf80143970
KenielDev <kenielalves3@gmail.com>
2023-06-09 18:39:43 -0300

    FEAT: Middleware para verificar se o usuário esta logado

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import {
@@ -12,6 +12,7 @@ import {
 } from "@heroicons/react/24/outline";
 import { usePathname } from "next/navigation";
 import Box from "../Box/Box";
+import { parseCookies } from "nookies";
 
 type NavItem = {
   label: string;
@@ -45,14 +46,14 @@ const Sidebar = ({ navItems = routes }: Props) => {
   const pathname = usePathname();
   const [isOpen, setIsOpen] = useState(false);
 
-  const Icon = isOpen ? ChevronDoubleRightIcon : ChevronDoubleLeftIcon;
+  const Icon = isOpen ? ChevronDoubleLeftIcon : ChevronDoubleRightIcon;
 
   const sidebarClass = `bg-secondary text-zinc-50 fixed md:static md:translate-x-0 z-0 transition-all duration-300 ease-in-out ${
-    !isOpen ? "w-[300px]" : "w-16"
+    isOpen ? "w-[300px]" : "w-16"
   } }`;
 
   const logoContainerClass = `flex items-center border-b border-b-indigo-800 transition-none ${
-    !isOpen ? "p-4 justify-between" : "py-4 justify-center"
+    isOpen ? "p-4 justify-between" : "py-4 justify-center"
   }`;
 
   const navListClass = `my-2 flex flex-col gap-2 items-stretch`;
@@ -61,6 +62,9 @@ const Sidebar = ({ navItems = routes }: Props) => {
     return null;
   }
 
+  const { user: user } = parseCookies();
+  const userData = JSON.parse(user);
+
   return (
     <div className={sidebarClass}>
       <div
@@ -68,7 +72,7 @@ const Sidebar = ({ navItems = routes }: Props) => {
       >
         {/* logo and collapse button */}
         <div className={logoContainerClass}>
-          {!isOpen && <img className="w-20" src="/img/logo.jpeg" />}
+          {isOpen && <img className="w-20" src="/img/logo.jpeg" />}
           <button
             className="grid place-content-center hover:bg-indigo-800 w-10 h-10 rounded-full opacity-0 md:opacity-100"
             onClick={() => setIsOpen(!isOpen)}
@@ -80,7 +84,7 @@ const Sidebar = ({ navItems = routes }: Props) => {
           <ul className={navListClass}>
             {navItems.map((item, index) => {
               const listItemClass = `text-light hover:bg-primary flex transition-colors duration-300 ${
-                !isOpen
+                isOpen
                   ? "rounded-xs p-2 mx-3 gap-4"
                   : "rounded-full p-2 mx-3 w-10 h-10"
               }`;
@@ -88,9 +92,9 @@ const Sidebar = ({ navItems = routes }: Props) => {
                 <li key={index} className={listItemClass}>
                   <Link
                     href={item.href}
-                    className={`flex gap-2 h-full ${!isOpen && "w-full"}`}
+                    className={`flex gap-2 h-full ${isOpen && "w-full"}`}
                   >
-                    {item.icon} <span>{!isOpen && item.label}</span>
+                    {item.icon} <span>{isOpen && item.label}</span>
                   </Link>
                 </li>
               );
@@ -99,16 +103,16 @@ const Sidebar = ({ navItems = routes }: Props) => {
         </nav>
         <div className={`grid place-content-stretch p-4`}>
           <div className="flex gap-4 items-center h-11 overflow-hidden">
-            <Image
-              src=""
+            <img
+              src={userData.avatar}
               height={36}
               width={36}
               alt="profile image"
               className="rounded-full"
             />
-            {!isOpen && (
+            {isOpen && (
               <div className="flex flex-col">
-                <span className="text-indigo-50 my-0">Tom Cook</span>
+                <span className="text-indigo-50 my-0">{userData.name}</span>
                 <Link href="/" className="text-indigo-200 text-sm">
                   View Profile
                 </Link>
