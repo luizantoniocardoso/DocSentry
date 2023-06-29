00dd41ed508711bafc1aaef237eb62b560c06bd7
Luiz-Antonio-Cardoso <la2004.o.sc@gmail.com>
2023-06-29 14:11:57 -0300

    adicionado rota de put pra usuario

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -22,6 +22,28 @@ export async function POST(req: Request, response:Response) {
     return NextResponse.json({res})
 } 
 
+export async function PUT(req: Request, response:Response) {
+    const { email, name, username, password, id, accessLevel, avatar } = await req.json() as User;
+
+    const res = await fetch(`http://localhost:3000/users/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ 
+            name,
+            email,
+            avatar,
+            username,
+            password,
+            accessLevel,
+
+        }),
+    })
+
+    return NextResponse.json({res})
+} 
+
 
 export async function GET(req: Request, response:Response) {
     const paramns = req.url.split('/')
