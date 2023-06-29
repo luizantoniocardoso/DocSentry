935d9cbcf4b5f34d870668a9e21fe6f0b3cf6912
Arthur Romansini Fernandes <59968944+ArthurRomansini@users.noreply.github.com>
2023-06-29 17:00:34 -0300

    feat: :sparkles: Tela de user
    Elaboracao da listagem de usuarios juntamente com modal para registro e alteracao de usuarios

---
+++ src/components/List/UserList.tsx
@@ -0,0 +1,66 @@
+"use client";
+import React, { use, useState } from "react";
+import ItemList from "./ItemList";
+import Typography from "../Typography/Typography";
+import Button from "../Buttons/Buttons";
+import RegisterModal from "../Modal/RegisterModal";
+import Input from "../Input/Input";
+import { PlusIcon } from "@heroicons/react/24/outline";
+import EditModal from "../Modal/EditModal";
+
+interface DocListProps {
+    // documents: DocType[];
+    users: any[];
+}  
+
+
+export default function UsersList( { users }: DocListProps): JSX.Element {
+
+  const [register, setRegister] = useState(false);
+  return (
+    <ul className="grid gap-4  w-full">
+          <div>
+                <Button onClick={() => setRegister(true)} className="flex gap-2 items-center">
+                <PlusIcon className="w-5" /> Adicionar Usuario
+                </Button>
+          </div>
+          <RegisterModal
+              isOpen={register}
+              onClose={() => setRegister(false)}
+            >
+            </RegisterModal>
+      {users.map((user) => {
+        const [isOpen, setIsOpen] = useState(false);
+
+        return (
+          <ItemList key={user.id}>
+            <div className="flex items-center justify-between">
+              <div>
+                <Typography>{user.name}</Typography>
+              </div>
+              <div>
+                <Button onClick={() => setIsOpen(true)}>Editar</Button>
+              </div>
+            </div>
+
+            <EditModal
+              title={`Editar ${user.name}`}
+              user={user}
+              isOpen={isOpen}
+              onClose={() => setIsOpen(false)}
+            >
+              <form className="mt-4 grid gap-2">
+                <Input
+                  type="text"
+                  label="Nome do Usuario"
+                  defaultValue={user.nome}
+                />
+                <Input type="File" label="userFoto" />
+              </form>
+            </EditModal>
+          </ItemList>
+        );
+      })}
+    </ul>
+  );
+}
